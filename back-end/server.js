f2af5e177f67607337910e614ebb07f4a439c473
Benjamin Kinard <benjaminjameskinard@gmail.com>
2022-03-30 22:17:20 +0000

    Get website functioning

---
+++ back-end/server.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+const mongoose = require('mongoose');
+
+// connect to the database
+mongoose.connect('mongodb://localhost:27017/travel', {
+  useNewUrlParser: true
+});
+
+
+// Configure multer so that it will upload to '../front-end/public/images'
+const multer = require('multer')
+const upload = multer({
+  dest: '/var/www/travel.benkwebs.com/images/',
+  limits: {
+    fileSize: 10000000
+  }
+});
+
+// Create a scheme for locations in the travel log: a name, date visited, and a path to an image.
+const locationSchema = new mongoose.Schema({
+  name: String,
+  dateVisit: String,
+  path: String,
+});
+
+// Create a model for locations in the travel log.
+const Location = mongoose.model('Location', locationSchema);
+
+// Upload a photo. Uses the multer middleware for the upload and then returns
+// the path where the photo is stored in the file system.
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+  // Just a safety check
+  if (!req.file) {
+    return res.sendStatus(400);
+  }
+  res.send({
+    path: "/images/" + req.file.filename
+  });
+});
+
+// Create a new location in the travel log: takes a name, date visited, and a path to an image.
+app.post('/api/locations', async (req, res) => {
+  const loc = new Location({
+    name: req.body.name,
+    dateVisit: req.body.dateVisit,
+    path: req.body.path,
+  });
+  try {
+    await loc.save();
+    res.send(loc);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Get a list of all of the locations in the travel log.
+app.get('/api/locations', async (req, res) => {
+  try {
+    let locations = await Location.find();
+    res.send(locations);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Delete a location from the travel log
+app.delete('/api/locations/:id', async (req, res) => {
+  try {
+    await Location.deleteOne({
+      _id: req.params.id
+    });
+    res.sendStatus(200);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+// Edit a location in the travel log
+app.put('/api/locations/:id', async (req, res) => {
+  try {
+    let loc = await Location.findOne({
+      _id: req.params.id
+    });
+    loc.name = req.body.name;
+    loc.dateVisit = req.body.dateVisit;
+    await loc.save();
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+});
+
+app.listen(3000, () => console.log('Server listening on port 3000!'));
---
+++ front-end/src/views/Admin.vue
@@ -0,0 +1,200 @@
+<template>
+<div class="admin">
+    <h1>The Admin Page!</h1>
+    <div class="heading">
+      <div class="circle">1</div>
+      <h2>Add an Item</h2>
+    </div>
+    <div class="add">
+      <div class="form">
+        <input v-model="name" placeholder="Location">
+        <p></p>
+        <textarea v-model="dateVisit" placeholder="Date of Visit"/>
+        <p></p>
+        <input type="file" name="photo" @change="fileChanged">
+        <button @click="upload">Upload</button>
+      </div>
+      <div class="upload" v-if="addLocation">
+        <h2>{{addLocation.name}}</h2>
+        <img :src="addLocation.path" />
+      </div>
+    </div>
+    <div class="heading">
+      <div class="circle">2</div>
+      <h2>Edit/Delete an Item</h2>
+    </div>
+    <div class="edit">
+      <div class="form">
+        <input v-model="findTitle" placeholder="Search">
+        <div class="suggestions" v-if="suggestions.length > 0">
+          <div class="suggestion" v-for="s in suggestions" :key="s.id" @click="selectLocation(s)">{{s.name}}
+          </div>
+        </div>
+      </div>
+      <div class="upload" v-if="findLocation">
+        <input v-model="findLocation.name">
+        <p></p>
+        <textarea v-model="findLocation.dateVisit"/>
+        <p></p>
+        <img :src="findLocation.path" />
+      </div>
+      <div class="actions" v-if="findLocation">
+        <button @click="deleteLocation(findLocation)">Delete</button>
+        <button @click="editLocation(findLocation)">Edit</button>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'AdminView',
+  data() {
+    return {
+      name: "",
+      dateVisit: "",
+      file: null,
+      addLocation: null,
+      locations: [],
+      findTitle: "",
+      findLocation: null,
+    }
+  },
+  computed: {
+    suggestions() {
+      let locations = this.locations.filter(loc => loc.name.toLowerCase().startsWith(this.findTitle.toLowerCase()));
+      return locations.sort((a, b) => a.name > b.name);
+    }
+  },
+  created() {
+    this.getLocations();
+  },
+  methods: {
+    async getLocations() {
+      try {
+        let response = await axios.get("/api/locations");
+        this.locations = response.data;
+        return true;
+      } catch (error) {
+        // Do nothing
+      }
+    },
+    selectLocation(loc) {
+      this.findTitle = "";
+      this.findLocation = loc;
+    },
+    async deleteLocation(loc) {
+      try {
+        await axios.delete("/api/locations/" + loc._id);
+        this.findLocation = null;
+        this.getLocations();
+        return true;
+      } catch (error) {
+        // Do nothing
+      }
+    },
+    async editLocation(loc) {
+      try {
+        await axios.put("/api/locations/" + loc._id, {
+          name: this.findLocation.name,
+          dateVisit: this.findLocation.dateVisit
+        });
+        this.findLocation = null;
+        this.getLocations();
+        return true;
+      } catch (error) {
+        // Do nothing
+      }
+    },
+    fileChanged(event) {
+      this.file = event.target.files[0]
+    },
+    async upload() {
+      try {
+        const formData = new FormData();
+        formData.append('photo', this.file, this.file.name)
+        let r1 = await axios.post('/api/photos', formData);
+        let r2 = await axios.post('/api/locations', {
+          name: this.name,
+          dateVisit: this.dateVisit,
+          path: r1.data.path
+        });
+        this.addLocation = r2.data;
+      } catch (error) {
+        // Do nothing 
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+.image h2 {
+  font-style: italic;
+  font-size: 1em;
+}
+
+.heading {
+  display: flex;
+  margin-bottom: 20px;
+  margin-top: 20px;
+}
+
+.heading h2 {
+  margin-top: 8px;
+  margin-left: 10px;
+}
+
+.add,
+.edit {
+  display: flex;
+}
+
+.circle {
+  border-radius: 50%;
+  width: 18px;
+  height: 18px;
+  padding: 8px;
+  background: #333;
+  color: #fff;
+  text-align: center
+}
+
+/* Form */
+input,
+textarea,
+select,
+button {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 1em;
+}
+
+.form {
+  margin-right: 50px;
+}
+
+/* Uploaded images */
+.upload h2 {
+  margin: 0px;
+}
+
+.upload img {
+  max-width: 300px;
+}
+
+/* Suggestions */
+.suggestions {
+  width: 200px;
+  border: 1px solid #ccc;
+}
+
+.suggestion {
+  min-height: 20px;
+}
+
+.suggestion:hover {
+  background-color: #5BDEFF;
+  color: #fff;
+}
+</style>
